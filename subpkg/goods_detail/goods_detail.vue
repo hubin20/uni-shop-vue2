e24dc37d0d7555161802857bde99538b6170ad1c
hubin1618 <hubin1618@qq.com>
2022-10-17 15:59:23 +0800

    '完成了首页功能的开发'

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    goosdetail
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
